a800379b82cda1ae8e5ae9261c3cb81b1f686905
Sidhartha Mishra <127133406+SidharthaMishra-lucky@users.noreply.github.com>
2024-03-21 02:05:54 +0530

    navbar-2:03:57

--- app/(dashboard)/_components/sidebar/new-button.tsx
+++ app/(dashboard)/_components/sidebar/new-button.tsx
@@ -8,6 +8,7 @@ import {
   DialogContent,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import { Hint } from "@/components/hint";
 
 
 export const NewButton = () => {
@@ -15,10 +16,16 @@ export const NewButton = () => {
     <Dialog>
       <DialogTrigger asChild>
         <div className="aspect-square">
-         
+          <Hint 
+          label="create organization"
+          side="right"
+          align="start"
+          sideOffset={18}
+          >
             <button className="bg-white/25 h-full w-full rounded-md flex items-center justify-center opacity-60 hover:opacity-100 transition">
               <Plus className="text-white" />
             </button>
+           </Hint> 
          </div>
       </DialogTrigger>
       <DialogContent className="p-0 bg-transparent border-none max-w-[480px]">
