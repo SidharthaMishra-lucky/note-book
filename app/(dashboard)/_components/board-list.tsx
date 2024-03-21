f0d2b5231fdda33406703c2e14f32ceafbc3f33f
Sidhartha Mishra <127133406+SidharthaMishra-lucky@users.noreply.github.com>
2024-03-21 17:12:45 +0530

    first-board-2:41

--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -1,10 +1,14 @@
 "use client";
 
+import { EmptyBoards } from "./empty-boards";
+import { EmptyFavorites } from "./empty-favorites";
+import { EmptySearch } from "./empty-search";
+
 interface BoardListProps {
     orgId: string;
     query: {
         search?: string;
-        favorite?: string;
+        favorites?: string;
     };
 };
 
@@ -12,6 +16,22 @@ export const BoardList = ({
     orgId,
     query,
 }: BoardListProps) => {
+    const data =[]; //todo 
+
+    if (!data?.length && query.search) {
+      return <EmptySearch />
+        }
+
+
+    if (!data?.length && query.favorites) {
+        return <EmptyFavorites />
+      }
+
+      if (!data?.length) {
+        return <EmptyBoards />
+      }
+
+
     return(
         <div>
               {JSON.stringify(query)}
