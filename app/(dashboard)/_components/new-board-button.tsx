32191e7a956dc64233a8e04c665dd21a718d75c9
Sidhartha Mishra <127133406+SidharthaMishra-lucky@users.noreply.github.com>
2024-03-26 22:01:36 +0530

    4:48:29

--- app/(dashboard)/_components/new-board-button.tsx
+++ app/(dashboard)/_components/new-board-button.tsx
@@ -5,6 +5,7 @@ import { Plus } from "lucide-react";
 import { api } from "@/convex/_generated/api";
 import { useApiMutation } from "@/hooks/use-api-mutation";
 import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
 interface NewBoardButtonProps {
     orgId: string;
@@ -15,6 +16,7 @@ export const NewBoardButton = ({
     orgId,
     disabled
 }: NewBoardButtonProps) => {
+    const router =useRouter();
     const {mutate, pending} = useApiMutation(api.board.create);
         
     const onClick = () => {
@@ -24,7 +26,7 @@ export const NewBoardButton = ({
             })
           .then((id) => {
             toast.success("Board created");
-            //todo
+            router.push(`/board/${id}`);
           })
           .catch(() =>toast.error("Failed to create board"));  
     };  
---
+++ app/board/[boardId]/page.tsx
@@ -0,0 +1,21 @@
+import { Room } from "@/components/room";
+
+import { Canvas } from "./_components/canvas";
+
+interface BoardIdPageProps {
+    params:{
+        boardId:string;
+    };
+};  
+
+const BoardIdPage = ({
+    params,
+}: BoardIdPageProps) => {
+ return (
+    <Room roomId={params.boardId}>
+    <Canvas boardId={params.boardId}/>
+    </Room>
+  );
+};
+
+export default BoardIdPage;
